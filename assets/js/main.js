307fb5b7fbeea9e6b041f75010a8e8210236e74e
Jp0liveira <joaooliveirawebdeveloper@gmail.com>
2023-04-03 11:12:56 -0300

    Style and Navigation Basic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,18 @@
 //Toggle mobile menu
+const menuToggleOpen = document.querySelector('.menu-toggle-open');
+const menuToggleClose = document.querySelector('.menu-toggle-close');
+const navMobileElement = document.querySelector('.nav-mobile');
 
+menuToggleOpen.addEventListener('click', () => navMobileElement.classList.add('active'));
+menuToggleClose.addEventListener('click', () => navMobileElement.classList.remove('active'));
 //Toggle theme
 
 //Show dropdown menu
+document.addEventListener('click', element => {
+    const dropdown = document.querySelector('.dropdown');
+    if (element.target.classList.contains('dropdown-btn')){
+        dropdown.classList.add('active');
+    }else{
+        dropdown.classList.remove('active');
+    }
+});
