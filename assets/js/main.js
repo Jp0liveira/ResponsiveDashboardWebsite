60577fdde4b903e1740c66766d046cd7e50d3de6
Jp0liveira <joaooliveirawebdeveloper@gmail.com>
2023-04-04 10:22:20 -0300

    theme control

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,24 @@ const navMobileElement = document.querySelector('.nav-mobile');
 
 menuToggleOpen.addEventListener('click', () => navMobileElement.classList.add('active'));
 menuToggleClose.addEventListener('click', () => navMobileElement.classList.remove('active'));
+
 //Toggle theme
+const bodyElement = document.body;
+const themeToggleBtn = document.querySelector('.theme-toggle-btn');
+const currentTheme = localStorage.getItem('currentTheme');
+
+if( currentTheme){
+    bodyElement.classList.add('theme-light');
+}
+
+themeToggleBtn.addEventListener('click', () => {
+    bodyElement.classList.toggle('theme-light');
+    if ( bodyElement.classList.contains('theme-light')){
+        localStorage.setItem('currentTheme', 'themeActive');
+    }else{
+        localStorage.removeItem('currentTheme', 'themeActive');
+    }
+});
 
 //Show dropdown menu
 document.addEventListener('click', element => {
